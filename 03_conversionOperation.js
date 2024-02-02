3a934e9be8a8bd835b042857d528d6ac2ba5c32a
SahilPatil7777 <sahilpatil32267@gmail.com>
2024-02-02 07:47:51 +0530

    operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -25,5 +25,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// 33 => it will convert to "33"
+
+// **********************************  Operations  ***********************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "hello";
+let str2 = " sahil";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //in this case it will join to string i.e 122
+// console.log(1 + 2 + "2"); // in this case it will first add 1 and 2 then join 3 with 2 i.e. 32
+
+// onsole.log(+true);//not good syntax
+// onsole.log(+"");//not good syntax
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; // not good syntax
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
