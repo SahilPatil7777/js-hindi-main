0e982d2ce2b93421cbf56dafb498a62f66df27a5
SahilPatil7777 <sahilpatil32267@gmail.com>
2024-02-02 07:17:15 +0530

    convertion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "sahil";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => it will be convert to 33
+// "33abc" => it will not convert and when you print give NaN
+// true => it will convert to 0 and false will convert to 1
+// sahil => it will not convert and when you print give NaN
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => it will convert to true and 0 => it will convert to false
+// "" => false
+// "hitesh" => it will convert to true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
