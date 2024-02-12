629be9f75d9130cba40f1bf39a6dc08afc79d7b9
SahilPatil7777 <sahilpatil32267@gmail.com>
2024-02-12 10:23:20 +0530

    map method and chaining

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,6 +1,6 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// callback function using map
+// callback function using map method
 // in map it return the values
 // const newNums = myNums.map((num) => num + 10);
 // const newNums = myNums.map((num) => { num + 10 });// when you open scope {}, return it.
