d41579daf918827c823cf61e8457a98c435190dc
SahilPatil7777 <sahilpatil32267@gmail.com>
2024-02-12 10:20:48 +0530

    chaining and map method

---
+++ 05_iterations/seven.js
@@ -0,0 +1,15 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// callback function using map
+// in map it return the values
+// const newNums = myNums.map((num) => num + 10);
+// const newNums = myNums.map((num) => { num + 10 });// when you open scope {}, return it.
+
+// console.log(newNums);
+
+// chaining: used two or more than two method's
+const newNums = myNums
+  .map((num) => num * 10)
+  .map((num) => num + 1)
+  .filter((num) => num > 40);
+console.log(newNums);
