2892b2d685408628157bc5aaea8a65466ba1b60b
SahilPatil7777 <sahilpatil32267@gmail.com>
2024-02-12 08:07:45 +0530

    forof loop

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -6,6 +6,37 @@
 
 const arr = [1, 2, 3, 4, 5];
 
+// forof loop on array
 for (const num of arr) {
-  console.log(num);
+  //   console.log(num);
+}
+
+const greetings = "hello world";
+// forof loop on string
+for (const greet of greetings) {
+  //   console.log(`each char is ${greet}`);
+}
+
+// Maps
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United State of America");
+map.set("FR", "France");
+map.set("IN", "India"); //it will not add india again because map is known for it's uniqueness
+
+// console.log(map);
+
+// forof loop on Map
+for (const [key, value] of map) {
+  //   console.log(key, ":-", value);
+}
+
+// forof loop on object : it will not work on object
+const myObj = {
+  game1: "NFS",
+  game2: "GTA",
+};
+
+for (const [key, value] of myObj) {
+  console.log(key, ":-", value);
 }
